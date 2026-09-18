40d0bdcf5e8be4c5bfb20a1160292ced618b78cf
sainify <sainify@users.noreply.github.com>
2026-09-18 18:31:24 +0000

    Connect HUSBA frontend to Worker API

--- apps/web/config.template.js
+++ apps/web/config.template.js
@@ -1,5 +1,5 @@
 window.HUSBA_CONFIG = Object.freeze({
-  API_BASE: __API_BASE_URL__,
+  API_BASE: https://husba-beads-api.husainsathi13.workers.dev,
   TURNSTILE_SITE_KEY: __TURNSTILE_SITE_KEY__,
   SITE_URL: __SITE_URL__,
   DEMO_MODE: __DEMO_MODE__,
